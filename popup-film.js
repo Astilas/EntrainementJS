2b51838265a89f27f3dc5116495ca0b6301e2278
Thibault Carayon <th.carayon@gmail.com>
2019-03-04 11:56:40 +0100

    second commit correction point- virgule

--- popup-film.js
+++ popup-film.js
@@ -3,7 +3,7 @@
 // Define some variables
 const nameMovie = 'Dragon 3 ';
 const nameRealisator  = ' Dean DeBlois ';
-const years = 2019
+const years = 2019;
 const message = nameMovie + 'réalisé par ' + nameRealisator + 'en ' + years;
 
 // Display a popup
